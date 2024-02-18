10bc970c6420c423d6a9982102989c27b6672361
Chenxinxu <1031307782@qq.com>
2024-02-18 22:23:24 +0800

    misc: move some file to test.

--- cpp-workspacegen2/app/main.cpp
+++ cpp-workspacegen2/app/main.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 using namespace std;
 
-#include "add.h"
-#include "dummy.h"
-#include "dummy2.h"
-
-
 int main(int argc, char** argv)
 {
-    Add addt(1, 2);
-    cout << addt.Calc() << endl;
-    print_msg("this is dummy");
-    print_msg2("this is dummy2");
-    cout << dumyi << endl;
-    #if defined(CXX_COMPILER_ID_MSVC)
-    cout << "cxx compiler is msvc..." << endl;
-    #endif
-    cout << "press Enter to continue..." << endl;
-    if (getchar()) {}
+    cout << "hello world" << endl;
     
     return 0;
 }
